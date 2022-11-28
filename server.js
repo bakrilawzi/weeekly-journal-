6af215e1811c0175bc2f44a852106d2e92b1add5
Baker Lawzi <bakricoder71@gmail.com>
2022-11-28 23:50:18 +0200

    edition 1

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app =  express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"))
+app.get("/",function(req,res){
+    res.render("main");
+});
+app.listen(3000,function(){
+    console.log("we're listening on the port 3000");
+})
